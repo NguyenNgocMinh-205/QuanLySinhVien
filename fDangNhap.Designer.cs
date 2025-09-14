1d575eb8842ff25a976b2dfd62d52e3d9d516b41
RuanMei^^ <ngocminh.cntt.cv@gmail.com>
2025-09-14 17:24:39 +0700

    Login

---
+++ fDangNhap.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace QuanLySinhVien
+{
+    partial class fDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.chkHienthiMatKhau = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập :";
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(30, 66);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(298, 30);
+            this.txtTenDangNhap.TabIndex = 0;
+            this.txtTenDangNhap.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(30, 124);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(298, 30);
+            this.txtMatKhau.TabIndex = 0;
+            this.txtMatKhau.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật khẩu :";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(122, 208);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(115, 34);
+            this.btnDangNhap.TabIndex = 2;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chkHienthiMatKhau
+            // 
+            this.chkHienthiMatKhau.AutoSize = true;
+            this.chkHienthiMatKhau.Location = new System.Drawing.Point(30, 172);
+            this.chkHienthiMatKhau.Name = "chkHienthiMatKhau";
+            this.chkHienthiMatKhau.Size = new System.Drawing.Size(169, 26);
+            this.chkHienthiMatKhau.TabIndex = 5;
+            this.chkHienthiMatKhau.Text = "Hiển thị mật khẩu";
+            this.chkHienthiMatKhau.UseVisualStyleBackColor = true;
+            this.chkHienthiMatKhau.CheckedChanged += new System.EventHandler(this.chkHienthiMatKhau_CheckedChanged);
+            // 
+            // fDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 292);
+            this.Controls.Add(this.chkHienthiMatKhau);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fDangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.CheckBox chkHienthiMatKhau;
+    }
+}
+
---
+++ fDangNhap.cs
@@ -0,0 +1,89 @@
+﻿using QLSV;
+using QuanLySinhVien.GUI;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public partial class fDangNhap : Form
+    {
+        private readonly connectData c = new connectData();
+
+        public fDangNhap()
+        {
+            InitializeComponent();
+            txtMatKhau.UseSystemPasswordChar = true;     // ẩn mật khẩu
+            this.AcceptButton = btnDangNhap;             // Enter = Đăng nhập
+        }
+
+        private void chkHienthiMatKhau_CheckedChanged(object sender, EventArgs e)
+        {
+            txtMatKhau.UseSystemPasswordChar = !chkHienthiMatKhau.Checked;
+        }
+
+        // Nút Đăng nhập (button1 / btnDangNhap)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = txtTenDangNhap.Text.Trim();
+            string pass = txtMatKhau.Text;
+
+            if (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(pass))
+            {
+                MessageBox.Show("Vui lòng nhập tên đăng nhập và mật khẩu.", "Thông báo");
+                return;
+            }
+
+            try
+            {
+                c.connect();
+
+                using (var cmd = new SqlCommand(
+                    "SELECT TenDangNhap, MatKhau, LoaiTaiKhoan FROM TaiKhoan WHERE TenDangNhap = @u", c.conn))
+                {
+                    cmd.Parameters.AddWithValue("@u", user);
+
+                    using (var rd = cmd.ExecuteReader())
+                    {
+                        if (rd.Read())
+                        {
+                            string dbPass = rd["MatKhau"]?.ToString() ?? "";
+                            if (string.Equals(dbPass, pass))
+                            {
+                                // Lưu phiên đăng nhập
+                                CurrentUser.Username = rd["TenDangNhap"].ToString();
+                                CurrentUser.Role = RoleUtils.NormalizeRole(rd["LoaiTaiKhoan"]?.ToString());
+
+                                // Mở trang chủ và áp quyền trong fTrangChu_Load (ApplyRole() không tham số)
+                                Hide();
+                                var main = new fTrangChu();
+                                main.Show();                          // fTrangChu_Load sẽ tự gọi ApplyRole()
+                                main.FormClosed += (_, __) => Close();
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu.", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi đăng nhập: " + ex.Message, "Thông báo");
+            }
+            finally { c.disconnect(); }
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // no-op
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // no-op
+        }
+
+    }
+}
+    
